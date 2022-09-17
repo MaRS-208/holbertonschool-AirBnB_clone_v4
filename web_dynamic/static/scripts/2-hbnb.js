d612d21ff901a44cf2f13e578f44d9a91d5e3de4
DaAsuncion <daniserafim_11@hotmail.com>
2022-09-17 15:02:28 +0000

    task2 all script and changes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,3 +13,14 @@ $(document).ready(function(){
     });
 
 });
+// api status
+  $(function () {
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+      if (String(textStatus) === String('success')) {
+	  	$('#api_status').addClass('available');
+	   } else {
+	    $('#api_status').removeClass('available');
+	   }
+    });
+  });
+});
